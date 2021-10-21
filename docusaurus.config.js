6e092f936880b6e7f07cd6a298cf4826726a7645
KumarAbhinav01 <abhinav.bitd@gmail.com>
2021-10-21 11:00:22 +0530

    Addition for Github pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,13 +8,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Documentation',
   tagline: 'Make documentations for your Project',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://Abhi6722.github.io',
+  baseUrl: '/Documentation/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Abhi6722', // Usually your GitHub org/user name.
+  projectName: 'Documentation', // Usually your repo name.
 
   presets: [
     [
